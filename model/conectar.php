1c9fadfd31efced872b6865c5829881ba20b41de
Marlon Brito dos Santos Castor <148072815+Marlon-Brito@users.noreply.github.com>
2025-01-27 05:28:51 -0300

    Update conectar.php

--- model/conectar.php
+++ model/conectar.php
@@ -1,6 +1,6 @@
 <?php
     // Conectando ao MySQL utilizando MySQLi
-    $sql = new MySQLi('sql305.infinityfree.com', 'if0_38182847', 'AomY4FYdDGhadM', 'if0_38182847_bd_otakeros'); 
+    $sql = new MySQLi('host', 'usuario', 'senha', 'banco-de-dados'); 
 
     // Falha ao conectar no banco de dados
     if($sql->connect_error){
